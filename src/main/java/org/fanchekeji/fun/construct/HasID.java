969a5824a0ea9576ccf563a7ecacaa2b51fd19bf
yuanrui01 <xdyrfree@gmail.com>
2023-05-23 23:59:19 +0800

    并发测试

---
+++ src/main/java/org/fanchekeji/fun/construct/HasID.java
@@ -0,0 +1,8 @@
+package org.fanchekeji.fun.construct;// concurrent/HasID.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+
+public interface HasID {
+  int getID();
+}
