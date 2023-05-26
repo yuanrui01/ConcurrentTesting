2af2c430105a6e566bde4f5e6a1c91e0a3b26d6a
yuanrui01 <xdyrfree@gmail.com>
2023-05-27 01:17:16 +0800

    并发测试

---
+++ src/main/java/org/fanchekeji/fun/lowlevel/Atomicity.java
@@ -0,0 +1,21 @@
+package org.fanchekeji.fun.lowlevel;// lowlevel/Atomicity.java
+// (c)2021 MindView LLC: see Copyright.txt
+// We make no guarantees that this code is fit for any purpose.
+// Visit http://OnJava8.com for more book information.
+import org.fanchekeji.fun.common.TimedAbort;
+
+import java.util.concurrent.*;
+
+public class Atomicity {
+  public static void test(IntTestable it) {
+    new TimedAbort(4, "No failures found");
+    CompletableFuture.runAsync(it);
+    while(true) {
+      int val = it.getAsInt();
+      if(val % 2 != 0) {
+        System.out.println("failed with: " + val);
+        System.exit(0);
+      }
+    }
+  }
+}
