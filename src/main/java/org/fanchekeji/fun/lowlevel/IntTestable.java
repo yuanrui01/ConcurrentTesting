805411e455dbbdf2f9d2a299eff8b11312783313
yuanrui01 <xdyrfree@gmail.com>
2023-05-27 20:07:28 +0800

    并发测试

--- src/main/java/org/fanchekeji/fun/lowlevel/IntTestable.java
+++ src/main/java/org/fanchekeji/fun/lowlevel/IntTestable.java
@@ -2,6 +2,7 @@ package org.fanchekeji.fun.lowlevel;// lowlevel/IntTestable.java
 // (c)2021 MindView LLC: see Copyright.txt
 // We make no guarantees that this code is fit for any purpose.
 // Visit http://OnJava8.com for more book information.
+import java.util.concurrent.TimeUnit;
 import java.util.function.*;
 
 public abstract class
@@ -10,5 +11,10 @@ IntTestable implements Runnable, IntSupplier {
   @Override public void run() {
     while(true)
       evenIncrement();
+  }
+
+  public static void main(String[] args) {
+    TimeUnit millseconds = TimeUnit.MILLISECONDS;
+    System.out.println(millseconds.toMicros(10));
   }
 }
